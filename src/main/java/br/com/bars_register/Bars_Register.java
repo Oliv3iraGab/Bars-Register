1c430f77bb6fb6fcd7d8dcc144474245d974f1cb
Gabriel Oliveira <limagabriel501@gmail.com>
2025-03-19 16:27:37 -0300

    Usando o FlatLaf como LookAndFeel

--- src/main/java/br/com/bars_register/Bars_Register.java
+++ src/main/java/br/com/bars_register/Bars_Register.java
@@ -1,7 +1,12 @@
 package br.com.bars_register;
 
 import br.com.bars_register.persistence.Produto;
+import br.com.bars_register.view.Login;
+import com.formdev.flatlaf.FlatIntelliJLaf;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -11,6 +16,16 @@ public class Bars_Register {
 
     public static void main(String[] args) {
 
+        try {
+            UIManager.setLookAndFeel(new FlatIntelliJLaf());
+            new Login().setVisible(true);
+        } catch (UnsupportedLookAndFeelException e) {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Failed to initialize LaF " + e.getMessage(),
+                    "ERRO",
+                    JOptionPane.WARNING_MESSAGE);
+        }
         Scanner s = new Scanner(System.in);
         // Protótipo de um registro de produto
         try {
