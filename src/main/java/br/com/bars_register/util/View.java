5410aca1410fe79c779afaeed7bf2cbeb86a249d
Gabriel Oliveira <limagabriel501@gmail.com>
2025-03-20 23:44:55 -0300

    Criação da classe e metodos util para view

---
+++ src/main/java/br/com/bars_register/util/View.java
@@ -0,0 +1,39 @@
+package br.com.bars_register.util;
+
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author limag
+ */
+public class View {
+
+    public static void standardCornerRadius(JPanel panel) {
+        panel.putClientProperty("FlatLaf.style",
+                "arc: 10");
+    }
+
+    public static void standardCornerRadius(JButton panel) {
+        panel.putClientProperty("FlatLaf.style",
+                "arc: 10");
+    }
+
+    public static void standardCornerRadius(JTextField panel) {
+        panel.putClientProperty("FlatLaf.style",
+                "arc: 10");
+    }
+
+    public static void standardCornerRadius(JScrollPane panel) {
+        panel.putClientProperty("FlatLaf.style",
+                "arc: 10");
+    }
+
+    public static void standardCornerRadius(JFormattedTextField table) {
+        table.putClientProperty("FlatLaf.style",
+                "arc: 10");
+    }
+}
