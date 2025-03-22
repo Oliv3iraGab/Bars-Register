cc2437029fc61c78a6b74fadc7ed4cf7e93cf013
Gabriel Oliveira <limagabriel501@gmail.com>
2025-03-22 00:04:00 -0300

    Criação da classe itemVenda

---
+++ src/main/java/br/com/bars_register/persistence/ItemVenda.java
@@ -0,0 +1,14 @@
+package br.com.bars_register.persistence;
+
+/**
+ *
+ * @author limag
+ */
+public class ItemVenda {
+    int id_venda;
+    int id_produto;
+    int quantidade;
+
+    public ItemVenda() {
+    }
+}
