e6a82c59e4b1fc1219f0e878e06a84397830fc0d
Gabriel Oliveira <limagabriel501@gmail.com>
2025-03-24 11:53:23 -0300

    Implementação das anotações do Lombok

--- src/main/java/br/com/bars_register/persistence/Produto.java
+++ src/main/java/br/com/bars_register/persistence/Produto.java
@@ -1,58 +1,23 @@
 package br.com.bars_register.persistence;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
 /**
  *
  * @author limag
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Produto {
-
     private int id;
     private String nome;
     private double preco;
     private int estoque;
-
-    public Produto() {
-    }
-
-    public Produto(int id, String nome, double preco, int estoque) {
-        this.id = id;
-        this.nome = nome;
-        this.preco = preco;
-        this.estoque = estoque;
-    }
-    
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
-
-    public int getEstoque() {
-        return estoque;
-    }
-
-    public void setEstoque(int estoque) {
-        this.estoque = estoque;
-    }
 
     public void atualizarEstoque() {
 
--- src/main/java/br/com/bars_register/persistence/Relatorio.java
+++ src/main/java/br/com/bars_register/persistence/Relatorio.java
@@ -1,28 +1,17 @@
 package br.com.bars_register.persistence;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
 /**
  *
  * @author limag
  */
+@Getter
+@Setter
+@NoArgsConstructor
 public class Relatorio {
     private int id;
     private String relatorio;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getRelatorio() {
-        return relatorio;
-    }
-
-    public void setRelatorio(String relatorio) {
-        this.relatorio = relatorio;
-    }
-    
-    
 }
