4a22de6663216ff2b186dcd00d0a819f109b90c0
Gabriel Oliveira <limagabriel501@gmail.com>
2025-03-23 23:12:13 -0300

    remoção do protótipo de registro de produto

--- src/main/java/br/com/bars_register/Bars_Register.java
+++ src/main/java/br/com/bars_register/Bars_Register.java
@@ -26,38 +26,5 @@ public class Bars_Register {
                     "ERRO",
                     JOptionPane.WARNING_MESSAGE);
         }
-        Scanner s = new Scanner(System.in);
-        // Protótipo de um registro de produto
-        try {
-            int id = 0;
-            do {
-                System.out.println("----Registro de produto----");
-                System.out.println("Digite o nome do produto: ");
-                String nomeProduto = s.nextLine();
-
-                System.out.println("Digite o preço do produto: ");
-                Double precoProduto = s.nextDouble();
-
-                System.out.println("Digite a quantidade em estoque do produto: ");
-                int estoqueProduto = s.nextInt();
-
-                s.nextLine();
-
-                Produto produto = new Produto(id, nomeProduto, precoProduto, estoqueProduto);
-
-                System.out.println("Produto registrado: " + produto.getNome());
-
-                System.out.println("Deseja informar outro produto? (S/N)");
-                String resposta = s.nextLine();
-
-                if (resposta.equalsIgnoreCase("N")) {
-                    break;
-                }
-            } while (true);
-        } catch (Exception e) {
-            System.out.println("Preencha os dados corretamente!");
-        } finally {
-            s.close();
-        }
     }
 }
