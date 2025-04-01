67e6f356adf39a1f246c586f6c8a771e447f18b6
Gabriel Oliveira <limagabriel501@gmail.com>
2025-03-31 21:19:40 -0300

    criação do form de registro de fornecedores e produtos

--- src/main/java/br/com/bars_register/view/register/RegistroProdutos.java
+++ src/main/java/br/com/bars_register/view/register/RegistroProdutos.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package br.com.bars_register.view;
+package br.com.bars_register.view.register;
 
 import br.com.bars_register.DAOClasses.ProdutoDAO;
 import br.com.bars_register.persistence.Produto;
