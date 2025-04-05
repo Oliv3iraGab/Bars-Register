175d0c9b4e90957e14c8c0bd0e21fc31f8be4c39
Gabriel Oliveira <limagabriel501@gmail.com>
2025-04-04 23:41:00 -0300

    criação da tela RegistroUsuarios e metodos para salvar e listar usuarios

--- src/main/java/br/com/bars_register/DAOClasses/UsuarioDAO.java
+++ src/main/java/br/com/bars_register/DAOClasses/UsuarioDAO.java
@@ -1,5 +1,7 @@
 package br.com.bars_register.DAOClasses;
 
+import java.util.ArrayList;
+
 import javax.swing.JOptionPane;
 import javax.swing.text.html.parser.Entity;
 
@@ -10,7 +12,39 @@ import jakarta.persistence.NoResultException;
 import jakarta.persistence.TypedQuery;
 
 public class UsuarioDAO {
-    public static Usuario validarUsuario(String login, String senha) {
+    
+   public static void salvarUsuario(Usuario usuario) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+           em.getTransaction().begin();
+               em.persist(usuario);
+           em.getTransaction().commit();
+           JOptionPane.showMessageDialog(null, "Usuário salvo com sucesso!");
+        } catch (Exception e) {
+           JOptionPane.showMessageDialog(null, "Erro ao salvar o usuário: " + e.getMessage()); 
+        } finally {
+           em.close();
+        }
+   }
+
+   public ArrayList<Usuario> listarUsuario() {
+        EntityManager em = JPAUtil.getEntityManager();
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        
+        try {
+            String jpql = "SELECT u FROM Usuario u ORDER BY u.nome";
+            TypedQuery<Usuario> query = em.createQuery(jpql, Usuario.class);
+            usuarios = new ArrayList<>(query.getResultList());
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar os usuários: " + e.getMessage());
+        } finally {
+            em.close();
+        }
+        return usuarios;
+   }
+
+   
+   public static Usuario validarUsuario(String login, String senha) {
         String jql = "SELECT u FROM Usuario u WHERE u.login = :login AND u.senha = :senha";
         EntityManager em = JPAUtil.getEntityManager();
         try {
